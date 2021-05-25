080c0851811587e5f158a93d16b1dd980e178ba8
rafaelhalarcon <rafaelalarcon@yahoo.com>
2021-05-25 13:23:12 -0600

    login page in frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,10 +2,39 @@ import { useState } from "react";
 
 const LoginForm = ({ username, password, onSave, saveError, saveButtonCaption }) => {
 
-let [ inputUsername, setInputUserName ] = useState(username)
-let [ inputPassword, setInputPassword ] = useState(password)
+    let [inputUsername, setInputUserName] = useState(username)
+    let [inputPassword, setInputPassword] = useState(password)
 
-async function onSaveClicked() {
-    console.log("User is loging in");
-    let updatedSignupForm = {
+    async function onSaveClicked() {
+        console.log("User is loging in");
+        let inputLogin = {
+            username: inputUsername,
+            password: inputPassword
+        }
+        console.log(inputLogin);
+        onSave(inputLogin);
+    }
+    const onInputChange = (event, setFunction) => {
+        console.log('Changing input to be ', event.target.value)
+        setFunction(event.target.value);
+    };
+
+    let loginUserProfileInvalid = !inputUsername || (updatedUsername.trim().lenght === 0);
+
+    return (
+        <div>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input id="username" value={inputUsername} onChange={(event) => onInputChange(event, setInputUserName)} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id="password" value={inputPassword} onChange={(event) => onInputChange(event, setInputPassword)} />
+            </div>
+            <button disabled={ loginUserProfileInvalid } onClick={ onSaveClicked }>{saveButtonCaption}</button>
+            { saveError && <div>{saveError}</div> }
+        </div>
+    )
 };
+
+export default LoginForm;
