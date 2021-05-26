38e8f2301f7057d6a0fa3af9be74a2c2deebd485
Patrick <patrickosaje@gmail.com>
2021-05-26 17:33:26 -0600

    login addon

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -77,6 +77,27 @@ const LoginPage = () => {
             cpsaStanding={userRecord?.cpsaStanding || UNKNOWN}
         />
     )
+
+    //return(
+      //  <div class="col-md-6 login-form-2">
+        //    <h3>Med-Com Login</h3>
+          //  <form>
+            //    <div class="form-group">
+              //      <input type="text" class="form-control" placeholder="Your Email *" value="" />
+        //         </div>
+        //         <div class="form-group">
+        //             <input type="password" class="form-control" placeholder="Your Password *" value="" />
+        //         </div>
+        //         <div class="form-group">
+        //             <input type="submit" class="btnSubmit" value="Login" />
+        //         </div>
+        //         <div class="form-group">
+
+        //             <a href="#" class="ForgetPwd" value="Login">Forget Password?</a>
+        //         </div>
+        //     </form>
+        // </div>
+//)
 }
 
 export default LoginPage;
