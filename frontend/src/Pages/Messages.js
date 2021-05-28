98637547c6f102036b5627aab3ee404401a09d44
Femi Bode-George <femibodgeorge@gmail.com>
2021-05-27 22:50:31 -0600

    Conversations progress

---
+++ frontend/src/Pages/Messages.js
@@ -0,0 +1,3 @@
+import { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
--- frontend/src/components/Messaging/ConversationList.js
+++ frontend/src/components/Messaging/ConversationList.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState } from "react";
+import './Conversation.css'
 
 
 const ConversationList = () => {
+   const ListOfConversations = []
+   
+    return (
+         <div className="" >
 
+
+    </div>
+    )
 }
+
+
+
+export default ConversationList
