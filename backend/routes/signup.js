3c3dbd5bc9bbc73041104555bf187321bdc8ebad
Femi Bode-George <femibodgeorge@gmail.com>
2021-06-01 22:52:08 -0600

    login working

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -6,17 +6,6 @@ const router = express.Router();
 //const User = require("../model/User");
 const authControl = require("../controllers/authControl");
 
-router.get("/", async(req, res) =>{
-    res.send("I am hitting homepage");
-})
-
-//Post function for sign-up form
-router.post('/register', authControl.register)
-
-
-router.post('/logout', authControl.logout)
-
-
 //router.post('/refresh_token', authControl.generateAccessToken)
 
 module.exports = router;
