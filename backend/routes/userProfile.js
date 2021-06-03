ea4847ab5be6cf9ebfa56871a65b621396668293
Patrick <patrickosaje@gmail.com>
2021-06-03 12:25:11 -0600

    userProfile working

--- backend/routes/userProfile.js
+++ backend/routes/userProfile.js
@@ -5,39 +5,59 @@ const Users = require("../model/User")
 const AuthMiddleware = require("../middlewares/authMiddleware")
 
 
-router.post("/getById/:id", AuthMiddleware, async (req, res) => 
-{
-    const profileId = req.authInfo.id
+// router.get("/getById/:id", async (req, res) => 
+// {
+//     const profileId = req.authInfo.id
 
-    const userProfile = new Users.findById(
-        {
-            ownerProfile:[profileId, userName, req.body.id, firstName, lastName]
-        })
+//     const userProfile = new Users.findBy_Id(
+//         {
+//             ownerProfile:[profileId, email, req.body.id, firstName, lastName]
+//         })
 
-        try 
+//         try 
+//         {
+//             const savedUser = await userProfile.save()
+//             res.status(200).json(savedUser)
+//         }
+//         catch(err)
+//         {
+//             res.status(500).json(err)
+//         }
+// })
+
+
+router.get("/getById/:id", async (req, res) => {
+    try 
         {
-            const savedUser = await userProfile.save()
-            res.status(200).json(savedUser)
+            const userProfile = await Users.findOne({ _id: req.params.id });
+            res.status(200).json(userProfile)
         }
         catch(err)
         {
             res.status(500).json(err)
         }
-})
-
-
-
-
-
-
-router.get("/getById/:id", async (req, res) => {
-    const userProfile = await Users.findOne({ _id: req.params.id });
   
-    if (!userProfile) {
-      return res.status(400).json({ msg: "This user does not exist." });
-    }
-    res.send(user);
+    // if (!userProfile) {
+    //   return res.status(400).json({ msg: "This user does not exist." });
+    // }
+    // res.send(user);
   });
+
+
+  //get a user
+// router.get("/", async (req, res) => {
+//     const userId = req.query.user;
+//     const username = req.query.userName;
+//     try {
+//       const user = userId
+//         ? await User.findById(userId)
+//         : await User.findOne({ username: username });
+//       const { password, updatedAt, ...other } = user._doc;
+//       res.status(200).json(other);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
 
 
   module.exports = router;
